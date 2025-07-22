a1701f71c78e4d52b21bf76f686ba09781e42e71
Sanucha Sondech <new.sanucha01@gmail.com>
2025-07-22 13:00:55 +0700

    Delete LabWeek5 directory

--- LabWeek5/Lab5_1/AuthorTest.java
+++
@@ -1,8 +0,0 @@
-package Lab5_1;
-
-public class AuthorTest {
-    public static void main (String [] args) {
-      Author a = new Author("Chanakarn Kingkaew","oakabc@gmail.com",'M'); //สร้าง object a สำหรับผู้แต่งชื่อ CK
-      System.out.println(a); 
-}
-} 
